c34cdf4f89d90f372988ab06710bf43ac5c72561
nyura123 <nyura123@gmail.com>
2020-02-10 00:29:04 -0600

    export more

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 export { AvailabilityCalendar } from './AvailabilityCalendar';
 export * from './utils';
 export * from './types';
+export * from './overrides';
 export { defaultComponents } from './overrides';
+export { AvailSlot } from './AvailSlot';
+export { AvailSlots } from './AvailSlots';
